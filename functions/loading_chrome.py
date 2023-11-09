ed7d8c8036151b3bd1a494e6ed560b1d35c8b1d0
Ali <39136692+arkh91@users.noreply.github.com>
2023-11-08 16:06:08 -0800

    Update loading_chrome.py
    Added url

--- functions/loading_chrome.py
+++ functions/loading_chrome.py
@@ -3,9 +3,9 @@ import subprocess   #Launch apps
 
 def launch_chrome():
     chrome_path = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-
+    url="https://apps.phsa.ca/logon/LogonPoint/tmindex.html"
     # Command to run Chrome
-    command = [chrome_path]
+    command = [chrome_path, url]
 
     # Launch Chrome
     subprocess.Popen(command)
